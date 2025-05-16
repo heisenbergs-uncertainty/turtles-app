ede2996c15856f282cee632e7352833235fc81ad
Matthew Holden <matthewreedholden@icloud.com>
2025-05-16 00:23:08 -0500

    chore(initialize): Initializing electron app

---
+++ electron/main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+  win.loadURL("http://localhost:5173"); // Vite dev server for React frontend
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
